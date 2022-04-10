2c61f16eafd6c87b3a0bbbcfc77b8df02d6fd24d
Alex <tornadoalex@hotmail.com>
2022-04-10 21:19:10 +0200

    project added

---
+++ C++/KeysValuesViews/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<ranges>
+#include<unordered_map>
+
+using namespace std;
+
+int main()
+{
+	unordered_map<string, int> wordsMap{ {"a", 1}, {"bc", 2}, {"abc", 3}, {"xyz", 4}};
+	auto keys = std::views::keys(wordsMap);
+	for (auto key : keys) { cout << key << " "; };
+
+	cout << endl;
+
+	auto values = std::views::values(wordsMap);
+	for (auto value : values) { cout << value << " "; };
+
+	cout << endl;
+
+	auto size_three_filt = [](const string& str) { return str.size() == 3; };
+	for (const auto& word : std::views::keys(wordsMap) | std::views::filter(size_three_filt))
+	{
+		cout << word << " ";
+	}
+
+	return 0;
+}
